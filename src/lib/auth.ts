cb0e393ddfea16043aa8157d5f209b6717947112
gabrielecavalletti <gabriele.cavalletti@driveagency.it>
2026-04-23 14:09:29 +0200

    fix: trustHost per deploy VPS e fix prisma config dotenv

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,6 +5,7 @@ import prisma from './prisma'
 import { z } from 'zod'
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  trustHost: true,
   session: { strategy: 'jwt' },
   pages: { signIn: '/login' },
   providers: [
